75c4aa99e2f32fbc978e7f3f3eec40495869cec6
meziani <meziani75011@gmail.com>
2025-09-17 16:45:07 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,27 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vueJsx(),
     vueDevTools(),
   ],
+  server: {
+    host: '127.0.0.1', // ← force IPv4
+    port: 5174,        // ← port fixe
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
